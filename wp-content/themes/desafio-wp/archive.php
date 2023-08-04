107a6313c356a826c523174934eb7a486b542026
Kyrsten Júnior <kyrstenjr@gmail.com>
2023-08-04 13:17:26 -0300

    Página de categoria povoada
    Falta apenas estilizar

--- wp-content/themes/desafio-wp/archive.php
+++ wp-content/themes/desafio-wp/archive.php
@@ -1,15 +1,32 @@
 <!-- Página de categoria -->
 <?php get_header(); ?>
 
-<section>
-  <div>
-    <?php
-      while(have_posts()){
-        the_post(); ?>
-        <h1><?php the_taxonomies() ?></h1>
-        <h2><?php the_title() ?></h2>
-      <?php } ?>
-  </div>
+<section id="pagina-categoria">
+
+  <?php if ( have_posts() ) : ?>
+
+  <h1><?php the_archive_title("<h1 class='titulo'>", "</h1>"); ?></h1>
+  <p><?php the_archive_description(); ?></p>
+
+  <?php while ( have_posts() ) : ?>
+    <?php the_post() ?>
+    <div class="miniatura">
+      <img src="<?php the_field('imagem_de_capa') ?>" alt="<?php the_title(); ?>">
+      <span class="duration"> <?php the_field('bx_play_video_duration') ?>m </span>
+      <?php the_title(sprintf( '<h3><a href="%s">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
+    </div>
+  <?php endwhile ?>
+
+  <?php endif ?>
+
+  <!-- <?php
+    $args = [
+      'post_type' => 'video'
+    ];
+    $query = new WP_Query($args);
+    while($query -> have_posts()){
+      $query -> the_post(); ?>
+  <?php } ?> -->
 </section>
 
 <?php get_footer(); ?>
